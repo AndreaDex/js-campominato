b0cc4061deae2185806c4d15307755337b55be2d
AndreaDex <desiderio.andrea@outlook.it>
2021-05-05 18:39:09 +0200

    complete bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,20 +14,32 @@ con difficoltà 1 => tra 1 e 80
 con difficoltà 2 => tra 1 e 50 */
 
 /**
- * Genera un numero casuale
+ * Genera un numero casuale tra un min ed  un max
  */
 function numeroRandom(min, max) {
     return Math.floor(Math.random() * (max - min +1) + min)  
 }
 
+
+var lvdifficolta = prompt("Scegli tra livello di diffoltà 0,1,2");
+var rangebombe ;
+if(lvdifficolta == 0){
+    rangebombe = 100;
+} else if(lvdifficolta == 1){
+    rangebombe = 80;
+} else if(lvdifficolta == 2){
+    rangebombe = 50;
+}
+
+
 //Array per le bombe generate casualmente
 var bombs = [];
 // Ciclo per inserire le bombe nell array 
 do {
-    numeroRandom(1, 100)
+    numeroRandom(1, rangebombe)
     //Controllo che le bombe non siano uguali
-    if (!bombs.includes(numeroRandom(1, 100))){
-        bombs.push(numeroRandom(1, 100))
+    if (!bombs.includes(numeroRandom(1, rangebombe))){
+        bombs.push(numeroRandom(1, rangebombe))
     }
     
 } while (bombs.length < 16);
@@ -36,18 +48,18 @@ console.log(bombs);
 //* Gioco
 
 var punti = 0;
-var possibilita = 3;
+var possibilita =   (rangebombe - 16);
 var disinneschi = [];
 
 do {
     //Chiedere all' utente di inserire un numero tra 1 e 100
-    var scelta = Number(prompt("Inserisci un numero"));
+    var scelta = Number(prompt("Inserisci un numero comreso tra 1 e " + rangebombe ));
 
-    //Verificare se input utente è un numero
+    //Verificare se input può essere inserito nell array disinneschi
     if(isNaN(scelta)){
         alert("Devi inserire un numero");
 
-    } else if (scelta < 1 || scelta >100) {
+    } else if (scelta < 1 || scelta > rangebombe) {
         alert("Devi inserire  un numero compreso tra 1 e 100");
 
     } else if (bombs.includes(scelta)){
